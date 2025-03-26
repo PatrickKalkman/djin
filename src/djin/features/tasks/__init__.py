37dfd3d1073719e2c50245116a72b8f788aaa0f6
Patrick Kalkman <patrick@simpletechture.nl>
2025-03-26 20:17:21 +0100

    refactor: Reorder Jira client imports alphabetically

--- src/djin/features/tasks/__init__.py
+++ src/djin/features/tasks/__init__.py
@@ -3,17 +3,17 @@ Jira task management features
 """
 
 from djin.features.tasks.jira_client import (
-    get_jira_client,
-    get_my_issues,
-    get_my_completed_issues,
-    get_issue_details,
-    display_issues,
     add_comment,
-    transition_issue,
+    assign_issue,
     create_issue,
     create_subtask,
-    assign_issue,
-    search_issues,
+    display_issues,
     get_available_transitions,
-    log_work
+    get_issue_details,
+    get_jira_client,
+    get_my_completed_issues,
+    get_my_issues,
+    log_work,
+    search_issues,
+    transition_issue,
 )
